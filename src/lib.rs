6f85e7a880a219e690edcc40ef54c8b0972d63aa
Stephen Sherratt <sfsherratt@gmail.com>
2018-09-01 19:28:36 +0100

    Flesh out low level parsing

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate either;
 extern crate getopts;
 
 use std::str::FromStr;
+use std::fmt::Display;
 
 pub mod ext;
 pub mod param;
@@ -10,14 +11,45 @@ pub use ext::{ParamExt, ParamOptExt};
 
 use param::*;
 
-pub fn param_req<T: FromStr>(long: &str) -> impl Param<Item = T> {
-    param(long, Required)
+pub fn arg_req<T: FromStr>(short: &str, long: &str, hint: &str, doc: &str) -> impl Param<Item = T> {
+    ArgSpec::new_from_str_required(short, long, hint, doc)
 }
-pub fn param_opt<T: FromStr>(long: &str) -> impl Param<Item = Option<T>> {
-    param(long, Optional)
+
+pub fn arg_opt<T: FromStr>(
+    short: &str,
+    long: &str,
+    hint: &str,
+    doc: &str,
+) -> impl Param<Item = Option<T>> {
+    ArgSpec::new_from_str_optional(short, long, hint, doc)
 }
-pub fn param_opt_def<T: FromStr + Clone>(long: &str, def: T) -> impl Param<Item = T> {
-    param(long, OptionalWithDefault { default: def })
+
+pub fn arg_opt_def<T: FromStr + Display + Clone>(
+    short: &str,
+    long: &str,
+    hint: &str,
+    doc: &str,
+    default: T,
+) -> impl Param<Item = T> {
+    let doc = doc.to_string();
+    ArgSpec::new_from_str_optional_with_default(
+        short,
+        long,
+        hint,
+        move |x| format!("{} {}", doc, x),
+        default,
+    )
+}
+
+fn example() {
+    let arg_spec = ArgSpec {
+        short: "f".to_string(),
+        long: "foo".to_string(),
+        hint: "value".to_string(),
+        doc: optional_with_default_doc_debug("a number"),
+        occur: optional_with_default(42),
+        convert: default_convert,
+    };
 }
 
 #[cfg(test)]
@@ -25,9 +57,9 @@ mod tests {
     use super::*;
     #[test]
     fn example1() {
-        let p = param_req::<i32>("int")
+        let p = arg_req::<i32>("int")
             .map(|i| i + 1)
-            .join(param_req::<f32>("float"));
+            .join(arg_req::<f32>("float"));
         let (i, f) = p.parse(&["--float", "6.5", "--int", "42"]).unwrap();
         assert_eq!(i, 43);
         assert_eq!(f, 6.5);
@@ -39,8 +71,8 @@ mod tests {
     }
     #[test]
     fn example2() {
-        let p_dimensions = param_opt::<i32>("width")
-            .opt_join(param_opt::<i32>("height"))
+        let p_dimensions = arg_opt::<i32>("width")
+            .opt_join(arg_opt::<i32>("height"))
             .opt_map(|(width, height)| Size::Dimensions(width, height));
         let p_fullscreen =
             flag("fullscreen").map(|f| if f { Some(Size::Fullscreen) } else { None });
--- src/param.rs
+++ src/param.rs
@@ -1,6 +1,8 @@
 use getopts;
 use std::ffi::OsStr;
 use std::str::FromStr;
+use std::marker::PhantomData;
+use std::fmt::{Debug, Display};
 
 #[derive(Debug)]
 pub enum TopLevelError<E> {
@@ -16,7 +18,7 @@ impl<T> From<getopts::Fail> for TopLevelError<T> {
 
 pub trait Param {
     type Item;
-    type Error: ::std::fmt::Debug;
+    type Error: Debug;
     fn update_options(&self, opts: &mut getopts::Options);
     fn get(&self, matches: &getopts::Matches) -> Result<Self::Item, Self::Error>;
     fn parse<C: IntoIterator>(&self, args: C) -> Result<Self::Item, TopLevelError<Self::Error>>
@@ -29,78 +31,235 @@ pub trait Param {
     }
 }
 
-pub struct Optional;
-pub struct Required;
+pub struct Optional<T>(PhantomData<T>);
+pub struct Required<T>(PhantomData<T>);
 pub struct OptionalWithDefault<T> {
-    pub default: T,
+    default: T,
 }
 
-pub trait Occur {}
+pub trait Occur {
+    type Item;
+    type Doc;
+}
 
-impl Occur for Optional {}
-impl Occur for Required {}
-impl<T> Occur for OptionalWithDefault<T> {}
+impl<T> Occur for Optional<T> {
+    type Item = T;
+    type Doc = String;
+}
+impl<T> Occur for Required<T> {
+    type Item = T;
+    type Doc = String;
+}
 
-pub struct Single<T, F: Fn(&str) -> Option<T>, O: Occur> {
-    long: String,
-    convert: F,
-    occur: O,
+pub type OptionalWithDefaultDoc<T> = Box<Fn(&T) -> String>;
+impl<T> Occur for OptionalWithDefault<T> {
+    type Item = T;
+    type Doc = OptionalWithDefaultDoc<T>;
+}
+pub fn optional_with_default_doc<T, F>(f: F) -> OptionalWithDefaultDoc<T>
+where
+    F: 'static + Fn(&T) -> String,
+{
+    Box::new(f)
+}
+pub fn optional_with_default_doc_display<T: Display>(doc: &str) -> OptionalWithDefaultDoc<T> {
+    let doc = doc.to_string();
+    optional_with_default_doc(move |x| format!("{} {}", doc, x))
+}
+pub fn optional_with_default_doc_debug<T: Debug>(doc: &str) -> OptionalWithDefaultDoc<T> {
+    let doc = doc.to_string();
+    optional_with_default_doc(move |x| format!("{} {:?}", doc, x))
+}
+
+pub fn optional<T>() -> Optional<T> {
+    Optional(PhantomData)
+}
+
+pub fn required<T>() -> Required<T> {
+    Required(PhantomData)
+}
+
+pub fn optional_with_default<T>(default: T) -> OptionalWithDefault<T> {
+    OptionalWithDefault { default }
+}
+
+pub struct ArgSpec<T, F: Fn(&str) -> Option<T>, O: Occur<Item = T>> {
+    pub short: String,
+    pub long: String,
+    pub hint: String,
+    pub doc: O::Doc,
+    pub occur: O,
+    pub convert: F,
+}
+
+pub fn default_convert<T: FromStr>(s: &str) -> Option<T> {
+    s.parse().ok()
+}
+
+impl<T, F: Fn(&str) -> Option<T>, O: Occur<Item = T>> ArgSpec<T, F, O> {
+    pub fn new(short: &str, long: &str, hint: &str, doc: O::Doc, occur: O, convert: F) -> Self {
+        Self {
+            short: short.to_string(),
+            long: long.to_string(),
+            hint: hint.to_string(),
+            doc,
+            occur,
+            convert,
+        }
+    }
+    fn convert(&self, string: &str) -> Option<T> {
+        (self.convert)(string)
+    }
+}
+impl<T: FromStr, O: Occur<Item = T>> ArgSpec<T, fn(&str) -> Option<T>, O> {
+    pub fn new_from_str(short: &str, long: &str, hint: &str, doc: O::Doc, occur: O) -> Self {
+        Self::new(short, long, hint, doc, occur, default_convert)
+    }
+}
+impl<T, F: Fn(&str) -> Option<T>, O: Occur<Item = T, Doc = String>> ArgSpec<T, F, O> {
+    fn doc(&self) -> String {
+        self.doc.clone()
+    }
+}
+impl<T, F: Fn(&str) -> Option<T>> ArgSpec<T, F, Required<T>> {
+    pub fn new_required(short: &str, long: &str, hint: &str, doc: &str, convert: F) -> Self {
+        Self::new(short, long, hint, doc.to_string(), required(), convert)
+    }
+}
+impl<T, F: Fn(&str) -> Option<T>> ArgSpec<T, F, Optional<T>> {
+    pub fn new_optional(short: &str, long: &str, hint: &str, doc: &str, convert: F) -> Self {
+        Self::new(short, long, hint, doc.to_string(), optional(), convert)
+    }
+}
+impl<T, F: Fn(&str) -> Option<T>> ArgSpec<T, F, OptionalWithDefault<T>> {
+    pub fn new_optional_with_default<D>(
+        short: &str,
+        long: &str,
+        hint: &str,
+        doc: D,
+        default: T,
+        convert: F,
+    ) -> Self
+    where
+        D: 'static + Fn(&T) -> String,
+    {
+        Self::new(
+            short,
+            long,
+            hint,
+            optional_with_default_doc(doc),
+            optional_with_default(default),
+            convert,
+        )
+    }
+    fn doc(&self) -> String {
+        (self.doc)(&self.occur.default)
+    }
+}
+impl<T: FromStr> ArgSpec<T, fn(&str) -> Option<T>, Required<T>> {
+    pub fn new_from_str_required(short: &str, long: &str, hint: &str, doc: &str) -> Self {
+        Self::new_required(short, long, hint, doc, default_convert)
+    }
+}
+impl<T: FromStr> ArgSpec<T, fn(&str) -> Option<T>, Optional<T>> {
+    pub fn new_from_str_optional(short: &str, long: &str, hint: &str, doc: &str) -> Self {
+        Self::new_optional(short, long, hint, doc, default_convert)
+    }
+}
+impl<T: FromStr> ArgSpec<T, fn(&str) -> Option<T>, OptionalWithDefault<T>> {
+    pub fn new_from_str_optional_with_default<D>(
+        short: &str,
+        long: &str,
+        hint: &str,
+        doc: D,
+        default: T,
+    ) -> Self
+    where
+        D: 'static + Fn(&T) -> String,
+    {
+        Self::new_optional_with_default(short, long, hint, doc, default, default_convert)
+    }
 }
 
 #[derive(Debug)]
-pub enum SingleError {
-    FailedToParse { long_flag: String, value: String },
+pub enum ArgError {
+    FailedToParse {
+        long: String,
+        hint: String,
+        value: String,
+    },
 }
 
-impl<T, F: Fn(&str) -> Option<T>> Param for Single<T, F, Required> {
+impl<T, F: Fn(&str) -> Option<T>> Param for ArgSpec<T, F, Required<T>> {
     type Item = T;
-    type Error = SingleError;
+    type Error = ArgError;
     fn update_options(&self, opts: &mut getopts::Options) {
-        opts.reqopt("", self.long.as_str(), "", "");
+        opts.reqopt(
+            self.short.as_str(),
+            self.long.as_str(),
+            self.doc().as_str(),
+            self.hint.as_str(),
+        );
     }
     fn get(&self, matches: &getopts::Matches) -> Result<Self::Item, Self::Error> {
-        let string = matches.opt_str(self.long.as_str()).unwrap();
-        (self.convert)(string.as_str()).ok_or_else(|| SingleError::FailedToParse {
-            long_flag: self.long.clone(),
-            value: string.clone(),
-        })
+        let string = matches
+            .opt_str(self.long.as_str())
+            .expect("this should be caught by getopts::Options::parse");
+        self.convert(string.as_str())
+            .ok_or_else(|| ArgError::FailedToParse {
+                long: self.long.clone(),
+                hint: self.hint.clone(),
+                value: string.clone(),
+            })
     }
 }
 
-impl<T: Clone, F: Fn(&str) -> Option<T>> Param for Single<T, F, OptionalWithDefault<T>> {
-    type Item = T;
-    type Error = SingleError;
-    fn update_options(&self, opts: &mut getopts::Options) {
-        opts.optopt("", self.long.as_str(), "", "");
-    }
-    fn get(&self, matches: &getopts::Matches) -> Result<Self::Item, Self::Error> {
-        match matches.opt_str(self.long.as_str()) {
-            Some(string) => {
-                (self.convert)(string.as_str()).ok_or_else(|| SingleError::FailedToParse {
-                    long_flag: self.long.clone(),
-                    value: string.clone(),
-                })
-            }
-            None => Ok(self.occur.default.clone()),
-        }
-    }
-}
-
-impl<T, F: Fn(&str) -> Option<T>> Param for Single<T, F, Optional> {
+impl<T, F: Fn(&str) -> Option<T>> Param for ArgSpec<T, F, Optional<T>> {
     type Item = Option<T>;
-    type Error = SingleError;
+    type Error = ArgError;
     fn update_options(&self, opts: &mut getopts::Options) {
-        opts.optopt("", self.long.as_str(), "", "");
+        opts.optopt(
+            self.short.as_str(),
+            self.long.as_str(),
+            self.doc().as_str(),
+            self.hint.as_str(),
+        );
     }
     fn get(&self, matches: &getopts::Matches) -> Result<Self::Item, Self::Error> {
         match matches.opt_str(self.long.as_str()) {
             Some(string) => (self.convert)(string.as_str())
-                .ok_or_else(|| SingleError::FailedToParse {
-                    long_flag: self.long.clone(),
+                .ok_or_else(|| ArgError::FailedToParse {
+                    long: self.long.clone(),
+                    hint: self.hint.clone(),
                     value: string.clone(),
                 })
                 .map(Some),
             None => Ok(None),
+        }
+    }
+}
+
+impl<T: Clone, F: Fn(&str) -> Option<T>> Param for ArgSpec<T, F, OptionalWithDefault<T>> {
+    type Item = T;
+    type Error = ArgError;
+    fn update_options(&self, opts: &mut getopts::Options) {
+        opts.optopt(
+            self.short.as_str(),
+            self.long.as_str(),
+            self.doc().as_str(),
+            self.hint.as_str(),
+        );
+    }
+    fn get(&self, matches: &getopts::Matches) -> Result<Self::Item, Self::Error> {
+        match matches.opt_str(self.long.as_str()) {
+            Some(string) => {
+                (self.convert)(string.as_str()).ok_or_else(|| ArgError::FailedToParse {
+                    long: self.long.clone(),
+                    hint: self.hint.clone(),
+                    value: string.clone(),
+                })
+            }
+            None => Ok(self.occur.default.clone()),
         }
     }
 }
@@ -120,17 +279,6 @@ impl Param for Flag {
     }
     fn get(&self, matches: &getopts::Matches) -> Result<Self::Item, Self::Error> {
         Ok(matches.opt_present(self.long.as_str()))
-    }
-}
-
-pub fn param<T: FromStr, O: Occur>(
-    long: &str,
-    occur: O,
-) -> Single<T, impl Fn(&str) -> Option<T>, O> {
-    Single {
-        long: long.to_string(),
-        convert: |s| s.parse().ok(),
-        occur,
     }
 }
 
